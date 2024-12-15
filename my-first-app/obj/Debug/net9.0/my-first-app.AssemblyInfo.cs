5a97ce78812d583ff1516c8a07c447fcbc0d6088
Sskiizens <sskiizens@mac.home>
2024-12-15 18:33:09 +0100

    modification projet ref et ajout test unitaire

--- my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
+++ my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f630dd3b2c9f6c76b9cb39f15defafce68f1d1e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
