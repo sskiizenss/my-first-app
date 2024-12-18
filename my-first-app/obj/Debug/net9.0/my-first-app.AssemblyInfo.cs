edfc912c4477d984cab6e5b0ed051150db7a0e46
Sskiizens <sskiizens@mac.home>
2024-12-18 15:30:19 +0100

    added CI

--- my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
+++ my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a97ce78812d583ff1516c8a07c447fcbc0d6088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9462a3fcf6bcfbd6b7792d6a868082c5edc99b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
