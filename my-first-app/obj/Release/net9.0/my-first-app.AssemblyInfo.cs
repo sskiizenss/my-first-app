0a09a1d198e75f4e4cb5470aeb9a7a77fc1cb237
Sskiizens <sskiizens@mac.home>
2024-12-22 19:07:49 +0100

    added dockerfile and configure server

--- my-first-app/obj/Release/net9.0/my-first-app.AssemblyInfo.cs
+++ my-first-app/obj/Release/net9.0/my-first-app.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9efb418987f892bbe702cd41a733dd06c12845")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
